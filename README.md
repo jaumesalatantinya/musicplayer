689b54897b3e8e8fb6d75b6c9f88e2510bc52999
Jaume Sala Tantinya <jaume.sala@scmspain.com>
2016-11-27 21:37:01 +0100

    Fix Readme

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 <li>npm install</li>
 <li>npm run start -s</li>
-<li>http://localhost:3000/</li>
-<li>npm run build</li>
+<li>npm scripts</li>
+<li>npm run start</li>
 <li>npm run dev</li>
 <li>npm run test</li>
 <li>npm run lint</li>
--- src/App.test.js
+++ src/App.test.js
@@ -7,23 +7,13 @@ describe('App Tests', () => {
 
 
     describe('Constructor', () => {
-        it('Should empty inicialize content and accordion', () => {
-            const app = new App();
-            expect(app.sections).to.exist;
-            expect(app.accordion).to.exist;
-
+        it('Should empty inicialize Music Player', () => {
         });
     });
-  
+
    describe('Init', () => {
 
         it('Should call getSectionsFromRenderedAccordion getSectionsFromApi', () => {
-            const app = new App();
-            spy(app, 'getSectionsFromRenderedAccordion');
-            spy(app, 'getSectionsFromApi');
-            app.init();
-            expect(app.getSectionsFromRenderedAccordion.called).to.be.true;
-            expect(app.getSectionsFromApi.called).to.be.true;
         });
     });
 });
